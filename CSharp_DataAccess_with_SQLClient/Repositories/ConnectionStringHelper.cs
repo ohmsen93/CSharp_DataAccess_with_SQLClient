de8975c18eda8a1a23e40ce1f7ea55927329325c
Mads Ohmsen <Ohmsen93@hotmail.com>
2023-02-20 17:32:41 +0100

    Nuget SqlClient and connectionStringHelper

---
+++ CSharp_DataAccess_with_SQLClient/Repositories/ConnectionStringHelper.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.Data.SqlClient;
+
+namespace CSharp_DataAccess_with_SQLClient.Repositories;
+
+public class ConnectionStringHelper
+{
+    public static string GetConnectionString()
+    {
+        SqlConnectionStringBuilder connectionStringBuilder = new SqlConnectionStringBuilder();
+        connectionStringBuilder.DataSource = "DESKTOP-1J1N5RM\\SQLEXPRESS";
+        connectionStringBuilder.InitialCatalog = "Chinook";
+        connectionStringBuilder.IntegratedSecurity = true;
+        connectionStringBuilder.TrustServerCertificate = true;
+        return connectionStringBuilder.ConnectionString;
+    }
+}
